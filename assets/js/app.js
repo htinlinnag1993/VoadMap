8ed4c2324a108b70500e62b277f402c234a33e8f
Credwa <craigroe7@hotmail.com>
2017-04-29 21:37:06 -0400

    first animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,27 @@
-//include express
-var express = require('express');
-//create an express application
-var app = express();
 
-var path = require('path');
-
-// app.use('/', express.static('assets'));
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.get('/',function(req, res, next){
-  res.sendFile(path.join(__dirname+'/main.html'));
-  //__dirname : It will resolve to your project folder.
+AFRAME.registerComponent('scale-on-mouseenter', {
+  schema: {
+    to: {default: '2.5 2.5 2.5'}
+  },
+  init: function () {
+    var data = this.data;
+    this.el.addEventListener('mouseenter', function () {
+      this.setAttribute('scale', data.to);
+    });
+  }
+});
+AFRAME.registerComponent('scale-on-mouseleave', {
+  schema: {
+    to: {default: '1 1 1'}
+  },
+  init: function () {
+    var data = this.data;
+    this.el.addEventListener('mouseleave', function () {
+      this.setAttribute('scale', data.to);
+    });
+  }
 });
 
-app.listen(8080, function () {
-      console.log('Example app listening on port 8080!');
+AFRAME.registerComponent('cameramove'), {
+
 });
